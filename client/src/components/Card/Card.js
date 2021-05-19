be06d7ae7bdf6a03182fa36b0b439b18e937939f
Gabriel <degiorgi.gabriel@gmail.com>
2021-05-19 18:07:13 -0300

    Se imprimen los nombres de los pokemons en las tarjetas

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Card.css"
 
-function Card() {
+function Card({pokemon}) {
     return (
         <div className="card">
-            Soy una Tarjeta
+            Nombre: {pokemon.name}
+    
         </div>
     )
 }
--- client/src/components/Cards/Cards.js
+++ client/src/components/Cards/Cards.js
@@ -1,19 +1,32 @@
-import React from 'react'
-import Card from "../Card/Card.js"
-import "./Cards.css"
+import React, { useEffect, useState } from "react";
+import Card from "../Card/Card.js";
+import "./Cards.css";
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { REACT_APP_BASE_URL, REACT_APP_POKEMONS } = process.env;
 
 function Cards() {
-    return (
-        <div className="cards"><h3 className="titulo">Estas son las tarjetas</h3>
-            <div className="cardsContainter">
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
-            </div>
-        </div>
-    )
+  const [pokemons, setPokemons] = useState([]); //Voy a guardar en un estado los pokemons que me llega. Al principio es un arreglo vacio
+
+  useEffect(() => {
+    axios.get(`${REACT_APP_BASE_URL}${REACT_APP_POKEMONS}`).then((response) => {
+      setPokemons(response.data);
+    });
+  }, []);
+  console.log(pokemons);
+  return (
+    <div className="cards">
+      <h3 className="titulo">Estas son las tarjetas</h3>
+      <div className="cardsContainter">
+        {pokemons.map((pokemon, index) => (
+          <Card key={index} pokemon={pokemon}/>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
