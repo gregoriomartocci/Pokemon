d1fc484ff151d4f8f316af5feed39f92e61863d0
Gregorio Martocci <gregoriomartocci@gmail.com>
2021-09-17 21:44:02 -0300

    deploy

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === "production") {
+  server.use(express.static(path.join(__dirname, "client/build")));
+}
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   console.log("Conexion con la base de datos correcta");
-  server.listen(3001, () => {
-    console.log("%s listening at 3001"); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log(`listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
